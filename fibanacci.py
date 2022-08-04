1acb6e23fd4b45d73bc1f06e0a8eb6e5b88612bf
johnjoseph <john@gmail.com>
2022-08-04 13:11:13 +0530

    UI update

--- fibanacci.py
+++ fibanacci.py
@@ -2,16 +2,16 @@ n1, n2 = 0, 1
 count = 0
 
 # check if the number of terms is valid
-if nterms <= 0:
+if numberOfterms <= 0:
    print("Please enter a positive integer")
 # if there is only one term, return n1
-elif nterms == 1:
-   print("Fibonacci sequence upto",nterms,":")
+elif numberOfterms == 1:
+   print("Fibonacci sequence upto",numberOfterms,":")
    print(n1)
 # generate fibonacci sequence
 else:
    print("Fibonacci sequence:")
-   while count < nterms:
+   while count < numberOfterms:
        print(n1)
        nth = n1 + n2
        # update values
